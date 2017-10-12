c0f4926917cd9fe8b313dc80b2034439c0f378bc
cosmosromantica <cosmosromantica@icloud.com>
2017-10-12 11:10:44 +0300

    Initial Commit

---
+++ strcalc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  strcalc
+//
+//  Created by Александр Гелета on 12.10.2017.
+//  Copyright © 2017 Александр Гелета. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
