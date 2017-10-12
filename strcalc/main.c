8111741070425fbde171bded6f9a23b02182be30
cosmosromantica <cosmosromantica@icloud.com>
2017-10-12 11:34:50 +0300

    Implemented user input for expression string.

--- strcalc/main.c
+++ strcalc/main.c
@@ -7,9 +7,24 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define ARGS_COUNT 3
+
+#define INPUT_ERROR_MSG                             \
+"Error while passing expression string.             \
+Please use:\n\tstrcalc <left> {+, -, *, /} <right>"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    double  left = 0.0f,            // left operand of the expression
+            right = 0.0f;           // right operand of the expression
+    char    operationChar;          // operation ASCII character
+    
+    if (fscanf(stdin, "%lf%c%lf", &left, &operationChar, &right) != ARGS_COUNT) {
+        fprintf(stderr, "%s", INPUT_ERROR_MSG);
+        return EINVAL;
+    }
+    
+    return EXIT_SUCCESS;
 }
